e3e16c25aa7b121cd95a03138bca1d2d5f981007
hdmetor <hdmetor@gmail.com>
2016-03-13 17:23:15 -0700

    check link for easy cases

--- main.py
+++ main.py
@@ -15,8 +15,22 @@ def check_link(link):
     if "|" not in link:
         # there is no alternate name in the link
         try:
-            r = requests.get('https://api.github.com/user'
+            url_link = format_link(link)
+            print("URL LINK ", url_link)
+            r = requests.get(ROOT.format(url_link))
+            if r.status_code is not 200:
+                raise NetworkError
+            print(r.text)
+        except NetworkError:
+            #log the eroor?
+            return None
+
+def format_link(link):
+    'properly format link to create a valid wikipedia url'
+
+    return link.rstrip().title().replace(" ", "_")
 
 
 if __name__ == '__main__':
-    find_links('raw.md')
+    #find_links('raw.md')
+    check_link('Algorithm')
