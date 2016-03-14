cd29c81d894146637677d9594f6393e430df44bb
hdmetor <hdmetor@gmail.com>
2016-03-13 19:18:46 -0700

    saving a local chache

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import requests
 import re
+import os
+
+DATA_FOLDER = 'data'
+if not os.path.isdir(DATA_FOLDER):
+    os.makedirs(DATA_FOLDER)
 
 ROOT = "https://en.wikipedia.org/w/index.php?title={}&action=raw"
 
@@ -14,7 +19,7 @@ def find_links(page, local=False, total=True):
         if not text:
             return
 
-    all_links = re.findall('\[\[(.*?)\]\]', text)[:20]
+    all_links = re.findall('\[\[(.*?)\]\]', text)
 
     if total:
         return len(all_links)
@@ -43,6 +48,11 @@ def clean_link(link):
     return url_link
 
 def connect(url_link):
+    # reading the local cache
+    local_path = build_local_path(url_link)
+    if os.path.exists(local_path):
+        print('reading from chache')
+        return read_local(local_path)
     redirect = True
     try:
         while redirect and url_link:
@@ -69,21 +79,34 @@ def connect(url_link):
     #print(text)
     if url_link:
     #print('usciti con ', url_link)
+        save_local(local_path, text)
         return text
     else:
         return None
 
+def build_local_path(url_link, folder=DATA_FOLDER):
+    return os.path.join(DATA_FOLDER, url_link)
+
+def save_local(path, text):
+    with open(path, 'wt') as fp:
+        fp.write(text)
+
+def read_local(path):
+    with open(path, 'rt') as fp:
+        text = fp.read()
+    return text
+
 def format_link(link):
     'properly format link to create a valid wikipedia url'
-
     return link.rstrip().replace(" ", "_")
 
 
 if __name__ == '__main__':
     all_links = map(clean_link, find_links('raw.md', local=True, total=False))
     #print(list(all_links)[2:3])
-    this = {k: find_links(k) for k in list(all_links)[2:3] if k and find_links(k)}
-    print(this)
+    this = {k: find_links(k) for k in list(all_links)[:5] if k and find_links(k)}
+    #print(len(this))
+    #save_local(build_local_path('aaaa'), 'this is the betx\nasokdjaoksdjaslkd\n')
     #check_link('Algorithm')
     #this = list(map(check_link, all_links[:12]))
     #print(this)
