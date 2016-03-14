a9d2b9efd5fa6102e56aa737639f99aeb68ae35d
hdmetor <hdmetor@gmail.com>
2016-03-13 22:00:06 -0700

    Adding logging & cleaning

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
-import requests
 import re
 import os
+import json
+import logging
+import requests
+from collections import namedtuple
 
+logger = logging.getLogger(__name__)
+
+ROOT = "https://en.wikipedia.org/w/index.php?title={}&action=raw"
 DATA_FOLDER = 'data'
 if not os.path.isdir(DATA_FOLDER):
     os.makedirs(DATA_FOLDER)
 
-ROOT = "https://en.wikipedia.org/w/index.php?title={}&action=raw"
-
-def find_links(page, local=False, total=True):
-    if not page:
+def find_links(text, total=True):
+    if not text:
         return None
-    if local:
-        with open(page, 'r') as fp:
-            text = fp.read()
-    else:
-        text = connect(page)
-        if not text:
-            return
-
     all_links = re.findall('\[\[(.*?)\]\]', text)
-
+    #print(all_links)
     if total:
         return len(all_links)
     else:
-        return all_links
+        return (clean_link(link) for link in all_links if clean_link(link))
 
 def clean_link(link):
 
@@ -42,43 +38,34 @@ def clean_link(link):
         try:
             assert len(link_parts) == 2
         except AssertionError:
-            # log the error
+            logger.debug('[WikiMarkDownSchemaError] {}'.format(link))
             return None
         url_link = format_link(link_parts[0])
     return url_link
 
-def connect(url_link):
+def find_text(url_link):
+
     # reading the local cache
     local_path = build_local_path(url_link)
     if os.path.exists(local_path):
-        print('reading from chache')
+
         return read_local(local_path)
     redirect = True
     try:
         while redirect and url_link:
-
-            #print("URL LINK ", url_link)
             r = requests.get(ROOT.format(url_link))
-            #print(ROOT.format(url_link))
             if r.status_code is not 200:
-                # note: s
                 raise requests.ConnectionError
-            #print("!!!!!!!")
             text = r.text
             if text.startswith("#REDIRECT"):
-                #print(text, )
                 url_link = clean_link(re.findall('\[\[(.*?)\]\]', text)[0])
 
             else:
                 redirect = False
-            # redirect
     except requests.ConnectionError:
-        print("@@@@@", url_link)
-            #log the eroor?
+        logger.debug('[ConnectionError] {}'.format(url_link))
         return None
-    #print(text)
     if url_link:
-    #print('usciti con ', url_link)
         save_local(local_path, text)
         return text
     else:
@@ -98,18 +85,24 @@ def read_local(path):
 
 def format_link(link):
     'properly format link to create a valid wikipedia url'
-    return link.rstrip().replace(" ", "_")
+    return link.rstrip().replace(" ", "_").replace("&minus;", "-")
 
 
 if __name__ == '__main__':
-    all_links = map(clean_link, find_links('raw.md', local=True, total=False))
-    #print(list(all_links)[2:3])
-    this = {k: find_links(k) for k in list(all_links)[:5] if k and find_links(k)}
-    #print(len(this))
-    #save_local(build_local_path('aaaa'), 'this is the betx\nasokdjaoksdjaslkd\n')
-    #check_link('Algorithm')
-    #this = list(map(check_link, all_links[:12]))
-    #print(this)
-    #that = [connect(l) for l in this]
-    #print(that)
-    #connect('Hopcroft–Karp_Algorithm')
+    first_text = read_local('raw.md')
+    all_links = find_links(first_text, total=False)
+
+    print('Gathering data...')
+
+    data = {\
+        k: {
+            "text_len" : len(find_text(k)),
+            "links" : find_links(find_text(k)) \
+            } for k in list(all_links) if k and find_links(find_text(k), total=True)}
+
+    print('Saving data...')
+
+    with open('final.json' , 'w') as fp:
+        json.dump(data, fp)
+
+    print('Saved')
